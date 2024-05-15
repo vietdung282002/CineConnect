478c40728d8f4980e886f9976e5de731cbc4960f
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-05-15 23:42:56 +0700

    commit

--- CineConnect/settings.py
+++ CineConnect/settings.py
@@ -151,8 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 AUTH_USER_MODEL = 'users.CustomUser'
-# STATIC_URL = "static/"
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # if DEBUG:
 #     STATICFILES_DIRS = [
 #         os.path.join(BASE_DIR, 'static')
