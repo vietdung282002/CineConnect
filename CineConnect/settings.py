4061bb422ed99efab21f5e32fb26c6424a2c187c
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-05-15 23:38:38 +0700

    commit

--- CineConnect/settings.py
+++ CineConnect/settings.py
@@ -151,17 +151,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 AUTH_USER_MODEL = 'users.CustomUser'
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static')
+#     ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
-MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+# MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
