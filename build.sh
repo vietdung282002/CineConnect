fe4f2417a60af59cab313eb3671e5ea25b620688
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-05-15 23:53:49 +0700

    commit

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -o errexit  # exit on error
 
 pip install -r requirements.txt
-
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
