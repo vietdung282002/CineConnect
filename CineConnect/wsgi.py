f98f3452821e3e286fd9a7b55e7e94829177960b
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-05-16 20:49:36 +0700

    azure

--- CineConnect/wsgi.py
+++ CineConnect/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'CineConnect.settings'
+settings_module = 'CineConnect.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'CineConnect.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
