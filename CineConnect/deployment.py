ab8b816610c0996e7e22ae7890a2f3ea081081e0
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-05-17 13:54:38 +0700

    azure

--- CineConnect/deployment.py
+++ CineConnect/deployment.py
@@ -33,7 +33,9 @@ STORAGES = {
     },
 }
 
-CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+CONNECTION = os.environ['TEST']
+
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
