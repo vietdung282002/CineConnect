914b7ae1ba69d50d69bca03a4c6ab660c9f4cfda
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-06-06 17:35:42 +0700

    follow

--- follow/serializers.py
+++ follow/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
 from .models import Follow
+from drf_spectacular.utils import extend_schema_field
+from user_profile.serializers import CustomUser, UserListSerializer
 
-class FollowSerializer(serializers.ModelSerializer):
+class FolloweeSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Follow
-        fields = ['followee']
+        fields = ['user']
+        
+    @extend_schema_field(serializers.ListField)
+    def get_user(self, follow_instance):
+        user = CustomUser.objects.get(id=follow_instance.followee.id)
+        context = self.context
+        return UserListSerializer(user,context=context).data
+    
+class FollowerSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Follow
+        fields = ['user']
+        
+    @extend_schema_field(serializers.ListField)
+    def get_user(self, follow_instance):
+        user = CustomUser.objects.get(id=follow_instance.follower.id)
+        context = self.context
+        return UserListSerializer(user,context=context).data
--- follow/views.py
+++ follow/views.py
@@ -1,40 +1,87 @@
 from django.shortcuts import render
 from rest_framework import viewsets,status,mixins,permissions
 from .models import Follow
-from .serializers import FollowSerializer
+from .serializers import FollowerSerializer,FolloweeSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from user_profile.serializers import UserListSerializer
+from users.models import CustomUser
 # Create your views here.
-class FollowViewSet(mixins.CreateModelMixin,
-                    mixins.DestroyModelMixin,
+class FollowViewSet(mixins.ListModelMixin,
                     viewsets.GenericViewSet):
     queryset = Follow.objects.all()
-    serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        followee = data.get('followee')
-        if Follow.objects.filter(follower_id=request.user.id, followee_id=followee).exists():
-            return Response({'message': 'Object already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_class(self):
+        
+        if self.action == 'list_following':
+            return FolloweeSerializer
+        else:
+            return FollowerSerializer
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['user_id'] = self.request.user.id
+        return context
+    
+    @csrf_exempt
+    @action(detail=False, methods=['post'])  
+    def toggle(self,request, *args, **kwargs):
+        context = super().get_serializer_context()
+        context['user_id'] = self.request.user.id
+        query = request.query_params.get('user', None)
+        user = CustomUser.objects.get(id=query)
         try:
-            follow = Follow.objects.create(follower_id=request.user.id, followee_id=followee)
+            follow = Follow.objects.get(follower_id= request.user.id, followee_id= query)
+            follow.delete()
             data = {
                 "status": "success",
-                "message": FollowSerializer(follow).data
+                "result": {
+                    'user':UserListSerializer(user,context= context).data,
+                    'isFollowing':False
+                }
             }
-            return Response(data, status=status.HTTP_201_CREATED)
-        except Exception as e:
-            data = {
-                "status": "error",
-                "message": str(e)
-            }
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
-    
-    def destroy(self, request, pk, *args, **kwargs):
-        try:
-            follow_obj = Follow.objects.get(follower_id=request.user.id, followee_id=pk)
-            follow_obj.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(data=data,status=status.HTTP_200_OK)
         except Follow.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            try:
+                follow = Follow.objects.create(follower_id= request.user.id, followee_id= query)
+                data = {
+                "status": "success",
+                "result": {
+                    'user':UserListSerializer(user,context= context).data,
+                    'isFollowing':True
+                }
+            }
+                return Response(data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                data = {
+                    "status": "error",
+                    "message": str(e)
+                }
+                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=['get'])  
+    def list_following(self, request, *args, **kwargs):
+        context = super().get_serializer_context()
+        context['user_id'] = self.request.user.id
+        query = request.query_params.get('user', None)
+        if query:
+            queryset = Follow.objects.filter(follower_id= query)
+            self.queryset= queryset
+        else:
+            self.queryset= []
+        return super().list(request, *args, **kwargs)
+    
+    @action(detail=False, methods=['get'])  
+    def list_follower(self, request, *args, **kwargs):
+        context = super().get_serializer_context()
+        context['user_id'] = self.request.user.id
+        query = request.query_params.get('user', None)
+        if query:
+            queryset = Follow.objects.filter(followee_id= query)
+            self.queryset= queryset
+        else:
+            self.queryset= []
+        return super().list(request, *args, **kwargs)
+    
