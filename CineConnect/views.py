b8ce6bb6f11f104f63962a39a6b921b5fc2a62b5
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-06-09 17:51:45 +0700

    test

--- CineConnect/views.py
+++ CineConnect/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 import os
 
 def index(request):
+    settings_module = 'CineConnect.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'CineConnect.settings'
+    
     env_vars = [f"{key}: {value}" for key, value in os.environ.items()]
-
+    html_output = settings_module
     # Tạo HTML cho danh sách
-    html_output = "<h1>Environment Variables:</h1>"
+    html_output += "<h1>Environment Variables:</h1>"
     html_output += "<ul>"
     for var in env_vars:
         html_output += f"<li>{var}</li>"
