2b6e05dd20594eafc7b13bbd261373340b58dc5c
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-06-11 03:30:45 +0700

    test

--- webjob/tasks.py
+++ webjob/tasks.py
@@ -1,12 +1,10 @@
 from .models import TestWJob
 from time import sleep
 import logging
-from background_task import background
 from django.utils import timezone
 
 logger = logging.getLogger(__name__)
 
-@background(schedule=5)
 def notify_user():
     # lookup user by id and send them a message
     logger.warning(str(timezone.now()))
